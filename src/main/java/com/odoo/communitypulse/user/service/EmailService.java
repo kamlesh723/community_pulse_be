cf41498948e1bab587f492bf7d1b33833d7eda5a
kamlesh723 <ky2673689@gmail.com>
2025-06-05 10:20:31 +0530

    FEAT [KY]: otp for register api

--- src/main/java/com/odoo/communitypulse/user/service/EmailService.java
+++ src/main/java/com/odoo/communitypulse/user/service/EmailService.java
@@ -1,6 +1,7 @@
 package com.odoo.communitypulse.user.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -16,7 +17,12 @@ public class EmailService {
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
-        mailSender.send(message);
+        try {
+            mailSender.send(message);
+        } catch (MailException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 
     public void sendOtpEmail(String to, String otp) {
